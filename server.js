60cb42de38943166b274a4840727b9a9375faca7
Cássio Zen <cassiozen@gmail.com>
2018-01-24 11:54:33 -0500

    sync update to reflect workshop

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const PORT = 3001;
 
 const init = async () => {
   //sync creates the table if it does not exist. alter true creates the tables and makes any changes to keep the modules in sync
-  await models.db.sync() 
+  await models.User.sync()
+  await models.Page.sync() 
   server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}!`);
   });
