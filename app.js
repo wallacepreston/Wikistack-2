f54c82db21bac1bf32490bb7a6cde1d207a67a31
Cássio Zen <cassiozen@gmail.com>
2018-01-26 16:23:14 -0500

    Removed custom error handler

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const path = require("path");
-const indexView = require("./views/index");
 
 app.use(morgan("dev")); //logging middleware
 app.use(express.static(path.join(__dirname, "./public"))); //serving up static files (e.g. css files)
@@ -15,12 +14,6 @@ app.use("/users", require("./routes/users"));
 
 app.get('/', function (req, res) {
    res.redirect('/wiki/');
-});
-
-//error handling middleware - MUST have all 4 parameters
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || "Internal Error");
 });
 
 module.exports = app;
