8f285154f6f6e00b962eac3b1682b885c642b827
Cássio Souza <cassiozen@gmail.com>
2018-03-13 11:25:23 -0400

    Update error.js

--- views/error.js
+++ views/error.js
@@ -2,7 +2,7 @@ const html = require("html-template-tag");
 const layout = require("./layout");
 
 module.exports = (message, error) => layout(html`
-  <h1>{{message}}</h1>
-  <h2>{{error.status}}</h2>
-  <pre>{{error.stack}}</pre>
+  <h1>${message}</h1>
+  <h2>${error.status}</h2>
+  <pre>${error.stack}</pre>
 `);
