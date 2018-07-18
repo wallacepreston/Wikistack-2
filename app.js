a375a150c8c00f1b30acd1b4ae943887069689e7
Thomas Kelly <tmkelly28@gmail.com>
2018-07-18 16:54:14 -0400

    remove body parser

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const path = require("path");
 
 app.use(morgan("dev")); //logging middleware
 app.use(express.static(path.join(__dirname, "./public"))); //serving up static files (e.g. css files)
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/wiki", require("./routes/wiki"));
 app.use("/users", require("./routes/users"));
